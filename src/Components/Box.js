4f45b641a99885c6cb6220ea051ee474cb0d0b14
Sujan Adhikari <Asujan205@gmail.com>
2022-10-12 23:02:43 +0545

    texture is added

--- src/Components/Box.js
+++ src/Components/Box.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
+import map from '../images/map.jpg'
 export default function Box(){
+    const texture = useLoader(TextureLoader, map)
     return <mesh rotation={[90,0,20]}>
         
         <boxBufferGeometry attach="geometry" args={[3,3,3]}/>
         <meshLambertMaterial attach="material" color="red"/>
+        <meshLambertMaterial attach="material" map={texture}/>
     </mesh>;
 }
