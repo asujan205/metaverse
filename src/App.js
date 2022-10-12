9e96332e4a234b65cf721b44eed849f39296359e
Sujan Adhikari <Asujan205@gmail.com>
2022-10-12 22:35:45 +0545

    boxisCreatedwithThreejs

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Web3 from 'web3';
 import {Canvas} from '@react-three/fiber'
-
+import Box from './Components/Box'
+import {OrbitControls} from '@react-three/drei'
 function App() {
   return (
     <div className="App">
-     <Canvas>
       
+     <Canvas className='Canvas'>
+      <OrbitControls enableZoom={false}/>
+      <ambientLight intensity={0.5}/>
+      <directionalLight position={[-2,5,2]} intensity={1}/>
+       <Box/>
      </Canvas>
     </div>
   );
